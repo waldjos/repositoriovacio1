bd7d8320bae06207c688151ba972ece156c156ef
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 00:53:06 -0400

    feat: enable automatic backups after invoice changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { initAutomaticBackup } from './cloudBackup'
 import './styles.css'
+
+initAutomaticBackup()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
