d70111cd6c5ab189be1c674af2b951534f934fd3
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 02:20:34 -0400

    chore: refresh PWA cache for rates module

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v2'
+const CACHE = 'zivifactura-v3'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
