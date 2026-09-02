c2789d2f5b4016ab611799dd25df88df42204d39
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 14:00:59 -0400

    Refresh PWA cache for Firestore voucher flow

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v12'
+const CACHE = 'zivifactura-v13'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
