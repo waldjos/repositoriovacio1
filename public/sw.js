7319a2a971347c71e52481e60856211c24d6eeeb
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 11:55:28 -0400

    chore: refresh PWA cache for email verification

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v8'
+const CACHE = 'zivifactura-v9'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
