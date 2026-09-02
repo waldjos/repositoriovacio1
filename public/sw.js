45eb341862cc03d6c7d26a57f1bb7ba19e45399e
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 02:44:28 -0400

    chore: refresh PWA cache for PDF options

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v4'
+const CACHE = 'zivifactura-v5'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
