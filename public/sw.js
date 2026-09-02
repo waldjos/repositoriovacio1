720cd9099411cab0dcc6f081f6f05f46dfd9c596
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 10:43:25 -0400

    chore: refresh PWA cache for receivables workspace

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v6'
+const CACHE = 'zivifactura-v7'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
