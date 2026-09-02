068703fa62b2587d0131c375935933e1feb99a09
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 02:29:38 -0400

    chore: refresh PWA cache for euro and copy updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v3'
+const CACHE = 'zivifactura-v4'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
