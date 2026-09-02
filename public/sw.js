ad31a9c9b54ef8fac074763c8ee3397e170944ee
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-02 03:01:44 -0400

    chore: refresh PWA cache for payment ledger

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v5'
+const CACHE = 'zivifactura-v6'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
