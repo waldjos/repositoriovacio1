761dec90b028a33c9d4c5e8980914bd3eca99e69
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-03 02:22:20 -0400

    Bump PWA cache for link sharing and multi-business

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zivifactura-v16'
+const CACHE = 'zivifactura-v17'
 const APP_SHELL = ['/', '/manifest.webmanifest?v=2', '/zivifactura-icon-v2.svg']
 
 self.addEventListener('install', (event) => {
