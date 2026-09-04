a5514b8d65a98d18650c29f8028632eb11e7e222
Joswald Lopez <113522558+waldjos@users.noreply.github.com>
2026-09-04 19:30:39 -0400

    feat: enable ZiviFactura v2 product chrome and light theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthShell from './AuthShell'
 import QuickTools from './QuickTools'
+import ZiviChrome from './ZiviChrome'
 import { initAutomaticBackup } from './cloudBackup'
 import './styles.css'
 import './payments.css'
 import './proofs.css'
 import './business.css'
+import './zivi-v2.css'
 
 initAutomaticBackup()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthShell />
+    <ZiviChrome />
     <QuickTools />
   </React.StrictMode>,
 )
